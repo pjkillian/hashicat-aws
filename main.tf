50af41378684532bd11c9c6954b6bd20f9ea555d
pjkillian <pkilli@yahoo.com>
2021-07-12 13:14:36 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -133,7 +133,7 @@ resource "aws_instance" "hashicat" {
 
   tags = {
     Name = "${var.prefix}-hashicat-instance"
-    Department = "devops2"
+    Department = "devops"
     Billable = "true"
   }
 }
